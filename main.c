8908584d6c04b93602fb59a59c96a61064a4f358
weiming <936450937@qq.com>
2019-12-10 23:28:15 +0800

    Signed-off-by: weiming <936450937@qq.com>

---
+++ main.c
@@ -0,0 +1,361 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+#define  MaxHeigh 53		//5+12*4
+#define  MaxWidth 88		//(10*4+5)*2
+#define	 BlockWidth 10	//一个Block宽度为10
+#define	 BlockHeigh 12	//一个Block高度为12
+
+
+/**
+定位坐标
+*/
+void gotoxy(int x, int  y) {
+	//调用win32api设置光标位置
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(handle, coord);
+}
+
+/**
+窗口最大化
+*/
+void full_screen()
+{
+	HWND hwnd = GetForegroundWindow();
+	int cx = GetSystemMetrics(SM_CXSCREEN);            /* 屏幕宽度 像素 */
+	int cy = GetSystemMetrics(SM_CYSCREEN);            /* 屏幕高度 像素 */
+
+	LONG l_WinStyle = GetWindowLong(hwnd, GWL_STYLE);   /* 获取窗口信息 */
+	/* 设置窗口信息 最大化 取消标题栏及边框 */
+	SetWindowLong(hwnd, GWL_STYLE, (l_WinStyle | WS_POPUP | WS_MAXIMIZE) & ~WS_CAPTION & ~WS_THICKFRAME & ~WS_BORDER);
+
+	SetWindowPos(hwnd, HWND_TOP, 0, 0, cx, cy, 0);
+}
+
+
+
+/**
+画边框
+*/
+void createLine()
+{
+	int i, j;
+	for (i = 0; i < MaxHeigh; i++)	//左右边框+中间
+	{
+		gotoxy(0, i);
+		printf("□");
+		gotoxy((MaxWidth) / 4, i);
+		printf("□");
+		gotoxy((MaxWidth) / 4 * 2, i);
+		printf("□");
+		gotoxy((MaxWidth) / 4 * 3, i);
+		printf("□");
+		gotoxy(MaxWidth, i);
+		printf("□");
+		printf("%d", i);						//行数标识
+	}
+	printf("\n");
+
+	for (j = 2; j <= MaxWidth; j += 2)	//上下边框+中间边框
+	{
+		gotoxy(j, 0);
+		printf("□");
+		gotoxy(j, (MaxHeigh - 1) / 4);
+		printf("□");
+		gotoxy(j, (MaxHeigh - 1) / 4 * 2);
+		printf("□");
+		gotoxy(j, (MaxHeigh - 1) / 4 * 3);
+		printf("□");
+		gotoxy(j, MaxHeigh - 1);
+		printf("□");
+
+	}
+	printf("\n");
+
+}
+
+/**
+设置颜色
+*/
+
+/**
+生成方块
+*/
+void  paintBlock(int x, int y) {
+	int  i, j;
+	int  rx = (x * BlockWidth + x + 1) * 2;//加上线和前面的块才是真正的x坐标，并且一个方块（线）占两个字符
+	int  ry = y * BlockHeigh + y + 1;
+
+	for (j = ry; j <= ry + BlockHeigh - 1; j++)
+	{
+		for (i = rx; i <= rx + BlockWidth * 2 - 1; i = i + 2)
+		{
+
+			gotoxy(i, j);
+			printf("■");
+		}
+	}
+	gotoxy(0, 52);
+
+}
+
+/**
+随机产生方块
+*/
+void  randCreateBlock(int bblock[]) {
+
+	int i;
+	for (i = 3; i >= 0; i--)
+	{
+
+		bblock[i] = rand() % 4;
+		paintBlock(bblock[i], i);
+	}
+}
+
+/**
+清除上次的痕迹
+*/
+void cleanBlock(int x, int y) {
+
+	int  i, j;
+
+	int  rx = (x * BlockWidth + x + 1) * 2;//加上线和前面的块才是真正的x坐标，并且一个方块（线）占两个字符
+	int  ry = y * BlockHeigh + y + 1;
+
+	for (j = ry; j <= ry + BlockHeigh - 1; j++)
+	{
+		for (i = rx; i <= rx + BlockWidth * 2 - 1; i = i + 2)
+		{
+			gotoxy(i, j);
+			printf("  ");
+		}
+	}
+	gotoxy(0, 55);
+
+}
+
+/**
+监听键盘按键
+*/
+void listenKeyBoard() {
+	if (_kbhit()) {
+		char ch = _getch();
+		printf("%c", ch);
+		printf("%d", ch);
+
+	}
+}
+
+/**
+读取最高分数
+*/
+int readmaxScore() {
+	int maxScore;
+	FILE* fp;
+	fopen_s(&fp, "score.txt", "r+");//r+读写 ，如果不存在就创建
+	fscanf_s(fp, "%d", &maxScore);
+	fclose(fp);
+	return maxScore;
+}
+
+/**
+绘制分数框
+*/
+
+void paintScoreBox(int score) {					//分数
+
+
+	int heigh = 15, width = 60;
+	int i;
+	//double duration;
+	//duration = (double)(finish - first) / CLOCKS_PER_SEC;
+
+	for (i = 100; i <= 100 + width; i = i + 2) {	//分数框上下
+		gotoxy(i, 20);
+		printf("☆");
+		gotoxy(i, 35);
+		printf("☆");
+	}
+	gotoxy(100, 20);
+	for (i = 20; i <= 35; i++) {	//分数框左右
+		gotoxy(100, i);
+		printf("☆");
+		gotoxy(100 + width, i);
+		printf("☆");
+	}
+	gotoxy(125, 28);
+	printf("YOUR SCORE:%d", score);	//显示当前分数
+	gotoxy(125, 25);
+	printf("最高分:%d", readmaxScore());	//显示最高分数
+	gotoxy(0, 53);
+	//printf("%f", duration);
+}
+
+/**
+保存最高分进文件
+*/
+void storeScore(int score)
+{
+	FILE* fp;
+	fopen_s(&fp, "score.txt", "r+");//w+读写 ，如果不存在就创建
+	fprintf(fp, "%d\n", score);
+	fclose(fp);
+}
+/**
+欢迎界面
+*/
+void welcome() {
+	//system("cls");
+	int n, flag = 1;
+	int i, j = 1;
+	while (flag == 1) {
+		gotoxy(43, 18);
+		printf("别 踩 白 块");
+		for (i = 20; i <= 26; i++)
+		{
+			for (j = 27; j <= 74; j++)
+			{
+				gotoxy(j, i);
+				if (i == 20 || i == 26)
+					printf("-");
+				else if (j == 27 || j == 74)
+					printf("|");
+			}
+		}
+		gotoxy(35, 22);
+		printf("1.开始游戏");
+		gotoxy(50, 22);
+		printf("2.游戏说明");
+		gotoxy(35, 25);
+		printf("3.退出游戏");
+
+		gotoxy(29, 27);
+		printf("请选择[1,2,3]:[ ]\b\b");
+		scanf_s("%d", &n);
+		switch (n) {
+		case 1:
+			system("cls");
+			createLine();
+			windowProgress();
+			flag = 0;
+			break;
+		case 2:
+			gotoxy(30, 30);
+			printf("从左到右依次对应四个键位:D F J K \n");
+			gotoxy(35, 35);
+			printf("输入任意键退出");
+			if (_getch()) {
+				system("cls");
+				break;
+			}
+
+		case 3:
+			exit(0);
+			break;
+		default:
+			gotoxy(30, 30);
+			printf("输入数据不符合条件");
+			gotoxy(35, 35);
+			printf("	输入任意键退出!   ");
+			if (_getch()) {
+				system("cls");
+				break;
+			}
+
+		}
+
+	}
+
+}
+
+/**
+窗口处理		1.清除痕迹 2.交换数据 3.重新绘制块 4.按键下移
+*/
+void  windowProgress() {
+	int i, j;	//方块位置向下
+	int tmp;
+	int score = 0;
+	int key[4] = { 100,102,106,107 };
+	int  bblock[4];						//四个黑块的位置
+
+
+	srand((unsigned int)time(NULL));    //时间种子，设置随机数用的	
+
+	randCreateBlock(bblock);			//产生块
+	clock_t start, first, finish;
+
+
+	while (1)
+	{
+		paintScoreBox(score);
+		tmp = bblock[3];
+		if (_getch() == key[tmp])
+		{
+			score++;
+			for (j = 0; j < 4; j++)
+			{
+				cleanBlock(bblock[j], j);		//清除痕迹
+			}
+
+			for (i = 3; i >= 1; i--)
+			{
+				bblock[i] = bblock[i - 1];	   //数据交换
+			}
+			bblock[0] = rand() % 4;
+
+			for (j = 0; j < 4; j++)		      //重新绘制
+			{
+				paintBlock(bblock[j], j);
+			}
+			gotoxy(0, 53);
+		}
+		else
+		{
+			if (score > readmaxScore())
+			{
+				printf("恭喜你创新纪录了！\n");
+				storeScore(score);
+			}
+			else
+			{
+				printf("叫你别踩白块啦！！\n\n");
+			}
+			int n;
+			printf("重新开始游戏按“1”，结束游戏按“2”,返回主菜单按“3”！\n\n");
+			scanf_s("%d", &n);
+			switch (n) {
+			case 1:
+				system("cls");
+				createLine();
+				windowProgress();
+				break;
+			case 2:
+				exit(0);
+				break;
+			case 3:
+				system("cls");
+				welcome();
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
+
+
+int main() {
+	full_screen();
+	system("color 70");		//设置背景，字颜色 
+	welcome();
+	return 0;
+}
+
+
